ec5b41b4e815fc3eb7a1bb649f26709e76eefe4c
mauricio moitino <maurimoitii@gmail.com>
2023-10-11 22:54:25 -0300

    el bucle de la function bingo, se dividio en dos bucles para cada jugador.

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
 // Math.floor(Math.random() * 100))  
+let arrPlayer = [];
+let arrCPU = [];
+let result = [];
+
 function bingo() {
-    let arrPlayer = [];
-    let arrCPU = [];
 
-    while(arrPlayer.length < 15 && arrCPU.length < 15) {
-
-        // generating number for player and CPU
-        let numPlayer = Math.floor(Math.random() * 90)
-        let numCPU = Math.floor(Math.random() * 90)
-
+    while(arrPlayer.length < 15) {
+        // generating number for playe
+        let numPlayer = Math.floor(Math.random() * 91) 
         // verifiying that the number is not repeated
         if(!arrPlayer.find((element) => element === numPlayer) && arrPlayer.length < 15) {
             arrPlayer.push(numPlayer);
+            console.log('Added to arrPlayer:', numPlayer);
         }
-
+    }
+    while(arrCPU.length < 15) { 
+        // generating number for CPU
+        let numCPU = Math.floor(Math.random() * 91) 
+        // verifiying that the number is not repeated
         if(!arrCPU.find((element) => element === numCPU) && arrCPU.length < 15) {
             arrCPU.push(numCPU);
+            console.log('Added to arrCPU:', numCPU);
         }
     }
     return {arrPlayer, arrCPU}
 }
+
+function changeContent () {
+    if(buttonStart.textContent === 'START') {
+
+    }
+}
+const buttonStart = document.querySelector(".bombo-button-start");
+buttonStart.addEventListener('click', bingo)
