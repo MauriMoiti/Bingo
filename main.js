30327e8b330801098a3e283656797bb871761d34
mauricio moitino <maurimoitii@gmail.com>
2023-10-11 21:30:29 -0300

    function con ciclo while generador de los 15 primeros numeros para cada jugador

--- main.js
+++ main.js
@@ -3,4 +3,20 @@ function bingo() {
     let arrPlayer = [];
     let arrCPU = [];
 
+    while(arrPlayer.length < 15 && arrCPU.length < 15) {
+
+        // generating number for player and CPU
+        let numPlayer = Math.floor(Math.random() * 90)
+        let numCPU = Math.floor(Math.random() * 90)
+
+        // verifiying that the number is not repeated
+        if(!arrPlayer.find((element) => element === numPlayer) && arrPlayer.length < 15) {
+            arrPlayer.push(numPlayer);
+        }
+
+        if(!arrCPU.find((element) => element === numCPU) && arrCPU.length < 15) {
+            arrCPU.push(numCPU);
+        }
+    }
+    return {arrPlayer, arrCPU}
 }
