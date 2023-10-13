65bd9a547d982afd347fafa30930cbed4db85d49
mauricio moitino <maurimoitii@gmail.com>
2023-10-12 21:17:51 -0300

    se creo un bucle Loop por medio de una function recursiva, que genera los numeros aleatorios, y luego los agrega al historial de numeros

--- main.js
+++ main.js
@@ -1,35 +1,135 @@
 // Math.floor(Math.random() * 100))  
 let arrPlayer = [];
 let arrCPU = [];
-let result = [];
+let arrResult = [];
+
+// select row-list player
+let playerRow1 = document.querySelector(".player-row-1")
+let playerRow2 = document.querySelector(".player-row-2")
+let playerRow3 = document.querySelector(".player-row-3")
+// select row-list CPU
+let cpuRow1 = document.querySelector(".cpu-row-1");
+let cpuRow2 = document.querySelector(".cpu-row-2");
+let cpuRow3 = document.querySelector(".cpu-row-3");
 
 function bingo() {
-
+let iPlayer = 0;
+let iCPU = 0;
     while(arrPlayer.length < 15) {
-        // generating number for playe
-        let numPlayer = Math.floor(Math.random() * 91) 
-        // verifiying that the number is not repeated
+        // Generating number for player
+        let numPlayer = Math.floor(Math.random() * 91) + 1;
+        // Verifiying that the number is not repeated
         if(!arrPlayer.find((element) => element === numPlayer) && arrPlayer.length < 15) {
             arrPlayer.push(numPlayer);
-            console.log('Added to arrPlayer:', numPlayer);
+            // Generating a span element for every aleatory number
+            if(playerRow1.children.length < 5) {
+                let span = document.createElement("span");
+                span.classList.add('number-row-1', 'number-player');
+                playerRow1.appendChild(span);
+                playerRow1.children[iPlayer].textContent = numPlayer;
+                iPlayer++
+            } else if(playerRow2.children.length < 5) {
+                if(iPlayer === 5) {
+                    iPlayer = 0
+                }
+                let span = document.createElement("span");
+                span.classList.add('number-row-2', 'number-player');
+                playerRow2.appendChild(span);
+                playerRow2.children[iPlayer].textContent = numPlayer;
+                iPlayer++
+            } else if(playerRow3.children.length < 5) {
+                if(iPlayer === 5) {
+                    iPlayer = 0
+                }
+                let span = document.createElement("span");
+                span.classList.add('number-row-3', 'number-player');
+                playerRow3.appendChild(span);
+                playerRow3.children[iPlayer].textContent = numPlayer;
+                iPlayer++
+            }
         }
     }
     while(arrCPU.length < 15) { 
         // generating number for CPU
-        let numCPU = Math.floor(Math.random() * 91) 
+        let numCPU = Math.floor(Math.random() * 91) + 1;
         // verifiying that the number is not repeated
         if(!arrCPU.find((element) => element === numCPU) && arrCPU.length < 15) {
             arrCPU.push(numCPU);
-            console.log('Added to arrCPU:', numCPU);
+            // Generating a span element for every aleatory number
+            if(cpuRow1.children.length < 5) {
+                let span = document.createElement("span");
+                span.classList.add('number-row-1');
+                cpuRow1.appendChild(span);
+                cpuRow1.children[iCPU].textContent = numCPU;
+                iCPU++
+            } else if(cpuRow2.children.length < 5) {
+                if(iCPU === 5) {
+                    iCPU = 0
+                }
+                let span = document.createElement("span");
+                span.classList.add('number-row-2');
+                cpuRow2.appendChild(span);
+                cpuRow2.children[iCPU].textContent = numCPU;
+                iCPU++
+            } else if(cpuRow3.children.length < 5) {
+                if(iCPU === 5) {
+                    iCPU = 0
+                }
+                let span = document.createElement("span");
+                span.classList.add('number-row-3');
+                cpuRow3.appendChild(span);
+                cpuRow3.children[iCPU].textContent = numCPU;
+                iCPU++
+            }
         }
     }
     return {arrPlayer, arrCPU}
 }
+const buttonStart = document.querySelector(".bombo-button");
 
-function changeContent () {
-    if(buttonStart.textContent === 'START') {
+// select container row results 
+let containerRow30 = document.querySelector(".container-result-30")
+let containerRow60 = document.querySelector(".container-result-60")
+let containerRow90 = document.querySelector(".container-result-90")
 
-    }
+// generates the numbers and adds them to the history 
+function changeContent() {
+        let i = 0;
+        function loop() {
+            if (arrResult.length < 90) {
+                let numberResult = Math.floor(Math.random() * 91) + 1;
+                arrResult.push(numberResult);
+                if (containerRow30.children.length < 30) {
+                    let span = document.createElement("span");
+                    span.classList.add('number-result');
+                    containerRow30.appendChild(span);
+                    containerRow30.children[i].textContent = numberResult;
+                    i++;
+                } else if(containerRow60.children.length < 30) {
+                    if(i === 30) {
+                        i = 0
+                    }
+                    let span = document.createElement("span");
+                    span.classList.add('number-result');
+                    containerRow60.appendChild(span);
+                    containerRow60.children[i].textContent = numberResult;
+                    i++;
+                } else if(containerRow90.children.length < 30) {
+                    if(i === 30) {
+                        i = 0
+                    }
+                    let span = document.createElement("span");
+                    span.classList.add('number-result');
+                    containerRow90.appendChild(span);
+                    containerRow90.children[i].textContent = numberResult;
+                    i++;
+                }
+                buttonStart.textContent = numberResult;
+                setTimeout(loop, 500); 
+            }
+        }
+        loop();  
 }
-const buttonStart = document.querySelector(".bombo-button-start");
-buttonStart.addEventListener('click', bingo)
+
+buttonStart.addEventListener('click', bingo);
+buttonStart.addEventListener('click', changeContent);
